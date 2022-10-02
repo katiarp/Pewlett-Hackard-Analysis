cf5186f0df33d9c115b7bfaed1365451c2a3c8f4
katiarp <katia.paredes84@gmail.com>
2022-10-02 15:32:22 -0700

    Added retirement_info.csv

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -50,7 +50,7 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, from_date, to_date)
 );
 
 SELECT * FROM employees;
